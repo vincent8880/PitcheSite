25c7f6a537af3bfe44479a2ebb3dd9536f09d7d8
vincent <ododovincent54@gmail.com>
2019-06-30 22:17:45 +0300

    made by start.sh file executable

--- start.sh
+++ start.sh
@@ -2,8 +2,11 @@ export SECRET_KEY=12345678
 export MAIL_USERNAME=ododovincent54@gmail.com
 export MAIL_PASSWORD=lionelandresmessi
 export DATABASE_PASS=12345
-export DATABASE_URL=
+export DATABASE_URL=postgresql+psycopg2://vincent:12345@localhost/pitchit_tests
 
-export MAIL_SERVER =
-export MAIL_PORT=
-export MAIL_USE_TLS=
+
+export MAIL_SERVER =smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=True
+
+python3.6 manage.py server
