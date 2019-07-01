81ebf433600961b6cdeb22de11379fc5cd749a85
vincent <ododovincent54@gmail.com>
2019-07-01 12:12:46 +0300

    updated my License file

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-export SECRET_KEY=12345678
-export MAIL_USERNAME=ododovincent54@gmail.com
-export MAIL_PASSWORD=lionelandresmessi
-export DATABASE_PASS=12345
-export DATABASE_URL=postgresql+psycopg2://vincent:12345@localhost/pitchit_tests
+export SECRET_KEY='12345678'
+export DATABASE_URL='postgresql+psycopg2://vincent:12345@localhost/pitchit_test'
 
 
-export MAIL_SERVER =smtp.googlemail.com
-export MAIL_PORT=587
-export MAIL_USE_TLS=True
 
 python3.6 manage.py server
