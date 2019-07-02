e7b3f55fa21d279d60473967887091a894823fc0
vincent <ododovincent54@gmail.com>
2019-07-02 14:34:40 +0300

    fin

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with general configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vincent:12345@localhost/eren'
      
 
 class DevConfig(Config):
